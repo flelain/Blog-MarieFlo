425f056bb4aff44aa6d980ba6f9ee8f81673f038
flelain <flelain@github.com>
2020-01-09 22:36:31 -0800

    modif format formulaire

--- subscribe.php
+++ subscribe.php
@@ -5,16 +5,16 @@ title: Subscribe
 
 <![Profile Image]({{ site.url }}/{{ site.picture }})!>
 <hr />
-<p>Si vous souhaitez être averti de la publication d'un nouveau billet sur le blog, laissez-nous vos nom/prénom et adresse email ci-dessous !</p>
-<p><i> If you wish to be notified as soon as we publish a new post, please leave your first/last names and email address below!</i></p>
+<p>Si vous souhaitez être averti de la publication d'un nouveau billet sur le blog, laissez-nous vos nom/prénom et adresse email ci-dessous !
+<i> If you wish to be notified as soon as we publish a new post, please leave your first/last names and email address below!</i></p>
 
   <form method="post" action="">
-    <b>Prénom/First name</b> : <input type="text" size="20" name="first_name" />
-    <br />
-    <b>Nom/Last name</b> : <input type="text" size="20" name="last_name" />
-    <br />
-    <b>Email</b> : <input type="email" size="20" name="email" />
-    <br />
+    <b>Prénom/First name</b> : <input type="text" name="first_name" />
+    <br /><br />
+    <b>Nom/Last name</b> : <input type="text" name="last_name" />
+    <br /><br />
+    <b>Email</b> : <input type="email" name="email" />
+    <br /><br />
     <input type="submit" value="Submit">
   </form>
 
