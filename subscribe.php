f3e10875e05e410e9c8cb388c3af80b80485324c
flelain <flelain@github.com>
2020-01-12 23:45:52 -0800

    Remise a jour subscribe

--- subscribe.php
+++ subscribe.php
@@ -61,11 +61,3 @@ title: Subscribe
       }
     }
   ?>
-
-<hr />
-<h2>About this site</h2>
-<p>This is a personal website, tailored to share our family adventures in Canada as well as professional-oriented projects. The site creation and maintenance overtime is in itself one of these projects, involving the five of us, especially in the content production!</p>
-
-<p>Many thanks to <a href="https://github.com/sergiokopplin/">Sergio Kopplin</a> and its minimalist <a href="https://github.com/sergiokopplin/indigo">Indigo Template</a> with which I quickly set up this website and learnt a lot in the same time!
-
-To know more about this site and what it is made of, please go to <a href="https://github.com/flelain">my github</a>.
